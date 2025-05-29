f388b8685487569d0189739e287d31f3dd103348
Johaneche <johan.manuel.echeverry@gmail.com>
2025-05-29 13:02:15 -0500

    Agregamos el JS para agregar o eliminar la clase popupOpened

--- pages/script.js
+++ pages/script.js
@@ -1,2 +1,36 @@
-let container = document.querySelector(".content");
-let;
+// Agregando las Clases a utilizar
+let popUp = document.querySelector(".popup");
+let nameDefault = document.querySelector(".main__paragraph_name");
+let occupationDefault = document.querySelector(".main__paragraph_occupation");
+
+// Agregando los Botones a utilizar
+let btnEdit = document.querySelector(".main__button_edit");
+let btnCloseWindow = popUp.querySelector(".popup__button_close");
+let btnSave = popUp.querySelector(".popup__button_save");
+
+// Agregando los Inputs
+let inName = document.querySelector(".popup__input_name");
+let inOccupation = document.querySelector(".popup__input_occupation");
+
+// Abrir la ventana de Editar
+function editWindow() {
+  popUp.classList.add("popup__opened"); // Agrego la clase al popup para que sea visible
+
+  inName.value = nameDefault.textContent; // Me aparezca el nombre del perfil a modificar
+  inOccupation.value = occupationDefault.textContent; // Me aparezca la ocupacion del perfil a modificar
+}
+
+// Cerrar la ventana de Editar
+function closeWindow() {
+  popUp.classList.remove("popup__opened"); // Elimino la clase al popup para que NO sea visible
+}
+
+function saveInfo() {
+  nameDefault.textContent = inName.value; //Cambio el nombre actual por el valor que hay el input
+  occupationDefault.textContent = inOccupation.value; //Cambio la ocupacion actual por el valor que hay el input
+  closeWindow();
+}
+
+btnEdit.addEventListener("click", editWindow);
+btnCloseWindow.addEventListener("click", closeWindow);
+btnSave.addEventListener("click", saveInfo);
